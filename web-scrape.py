2ca96338e52b3cb1b917ce49c9a537eaefb563a2
Gage Weaver <Gage_Weaver@ku.edu>
2024-04-27 23:10:04 -0500

    Update web-scrape.py
    Updates to list names

--- web-scrape.py
+++ web-scrape.py
@@ -1,7 +1,7 @@
 import requests  # Import requests to access webpage
 from bs4 import BeautifulSoup  # Import BeautifulSoup to parse webpage
 import pandas as pd  # Import pandas to create DataFrame
-years = list(range(1990, 2024))  # Define list of years to search
+years = list(range(1990, 2025))  # Define list of years to search
 url = 'https://www.basketball-reference.com/awards/NBA_{}.html'  # Define base URL
 """ This only runs once, to avoid making too many requests to the website, do same for player stats later (not shown)
 for year in years:  # Iterate through years
@@ -10,7 +10,7 @@ for year in years:  # Iterate through years
     with open("mvp/{}.html".format(year), "w+") as file:  # Open file to write data
         file.write(data.text)  # Write data to file
 """
-dataframes = []  # Create empty list to store DataFrames
+dataframes1 = []  # Create empty list to store DataFrames
 """ This only runs once, to avoid long run times creating the mvps.csv file
 for year in years:  # Iterate through years
     with open("mvp/{}.html".format(year), encoding='utf-8') as f:  # Open file
@@ -20,11 +20,12 @@ for year in years:  # Iterate through years
     mvptable = soup.find(id='mvp')  # Find MVP table
     mvp = pd.read_html(str(mvptable))[0]  # Read table into DataFrame
     mvp['Year'] = year  # Add year column to identify year of winner
-    dataframes.append(mvp)  # Append DataFrame to list
-mvps = pd.concat(dataframes)  # Concatenate DataFrames to one frame
+    dataframes1.append(mvp)  # Append DataFrame to list
+mvps = pd.concat(dataframes1)  # Concatenate DataFrames to one frame
 mvps.to_csv('mvps.csv')  # Save DataFrame to CSV file
 """
-""" This only runs once, to avoid long run times, creating the players.csv file
+dataframes2 = []
+"""
 for year in years:  # Iterate through years
     with open("player/{}.html".format(year), encoding='utf-8') as f:  # Open file
         page = f.read()  # Read webpage
@@ -33,10 +34,11 @@ for year in years:  # Iterate through years
         playertable = soup.find(id='per_game_stats')  # Find player table
         player = pd.read_html(str(playertable))[0]  # Read table into DataFrame
         player['Year'] = year  # Add year column to identify year of stats
-        dataframes.append(player)
-players = pd.concat(dataframes)  # Concatenate DataFrames to one frame
+        dataframes2.append(player)
+players = pd.concat(dataframes2)  # Concatenate DataFrames to one frame
 players.to_csv('players.csv')  # Save DataFrame to CSV file
 """
+dataframes3=[]
 for year in years:  # Iterate through years
     with open("team/{}.html".format(year), encoding='utf-8') as f:  # Open file
         page = f.read()  # Read webpage
@@ -47,7 +49,7 @@ for year in years:  # Iterate through years
     team['Year'] = year  # Add year column to identify year of winner
     team['Team'] = team['Eastern Conference']
     del team['Eastern Conference']
-    dataframes.append(team)  # Append DataFrame to list
+    dataframes3.append(team)  # Append DataFrame to list
     soup = BeautifulSoup(page, 'html.parser')  # Parse webpage
     table = soup.find('tr', class_='thead').decompose()  # Find redundant row and remove
     teamtable = soup.find(id='divs_standings_W')  # Find MVP table
@@ -55,7 +57,7 @@ for year in years:  # Iterate through years
     team['Year'] = year  # Add year column to identify year of winner
     team['Team'] = team['Western Conference']
     del team['Western Conference']
-    dataframes.append(team)  # Append DataFrame to list
-teams = pd.concat(dataframes)  # Concatenate DataFrames to one frame
+    dataframes3.append(team)  # Append DataFrame to list
+teams = pd.concat(dataframes3)  # Concatenate DataFrames to one frame
 teams.to_csv('teams.csv')  # Save DataFrame to CSV file
 
